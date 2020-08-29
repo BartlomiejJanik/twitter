22676796d0cd862f6ccb12803aeca323e1b3be51
Bartlomiej Janik <yanekk1990@gmail.com>
2020-08-29 14:58:58 +0200

    implementacja procesu logowania

---
+++ src/main/java/pl/sda/twitter/services/UserService.java
@@ -0,0 +1,13 @@
+package pl.sda.twitter.services;
+
+import pl.sda.twitter.persistance.entiities.TbUser;
+
+public class UserService {
+
+    public TbUser getUser(String userName, String userPassword) {
+        if (userName.equals("admin") && userPassword.equals("password")) {
+            return TbUser.builder().login(userName).password(userPassword).build();
+        }
+        return null;
+    }
+}
